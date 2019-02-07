3042bbdfa83161a6981a21f362ad6c0f69933c6d
Yoni Hirshowitz <jonhirsh39@gmail.com>
2019-02-07 09:35:17 -0500

    added swagger

---
+++ src/main/java/com/yonipony/app/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.yonipony.app;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	Contact contact = new Contact("yonipony", "http://www.yonipony.com", "jonhirsh39@gmail.com");
+	List<VendorExtension> vendorExtension = new ArrayList<>();
+	ApiInfo apiInfo = new ApiInfo("My first Spring Boot", "This page documents RESTful Web Service endpoints", "1.0",
+			"localhost:8080", contact, "Apache 2.0", "http://www.apache.org/license/LICENSE-2.0", vendorExtension);
+
+	@Bean
+	public Docket apiDocket() {
+		Docket docket = new Docket(DocumentationType.SWAGGER_2)
+				// .protocols(new HashSet<>(Arrays.asList("HTTP", "HTTPS")))
+				.apiInfo(apiInfo).select().apis(RequestHandlerSelectors.basePackage("com.yonipony.app"))
+				.paths(PathSelectors.any()).build();
+
+		return docket;
+	}
+}
--- src/main/java/com/yonipony/app/security/WebSecurity.java
+++ src/main/java/com/yonipony/app/security/WebSecurity.java
@@ -31,8 +31,9 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable().authorizeRequests()
 				.antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
-				.antMatchers(SecurityConstants.H2_CONSOLE).permitAll().anyRequest().authenticated().and()
-				.addFilter(new AuthenticationFilter(authenticationManager()))
+				.antMatchers(SecurityConstants.H2_CONSOLE).permitAll()
+				.antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
+				.anyRequest().authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()))
 				.addFilter(new AuthorizationFilter(authenticationManager())).sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
@@ -42,6 +43,12 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+	}
+	
+	protected AuthenticationFilter getAuthenticationFilter() throws Exception {
+	    final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+	    filter.setFilterProcessesUrl("/users/login");
+	    return filter;
 	}
 
 	@Bean
